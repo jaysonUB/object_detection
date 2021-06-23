645a19291baa1b7ee1db02a96e0ddff6fdbf5a09
Ian Kahwai <iankahwai@gmail.com>
2021-06-23 15:43:45 +0200

    Initialize repo

---
+++ detection.py
@@ -0,0 +1,89 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+from keras.preprocessing.image import img_to_array
+from matplotlib import pyplot as plt
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import os
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+
+path = os.getcwd()
+STATIC_FOLDER = os.path.join(path, 'static')
+if not os.path.isdir(STATIC_FOLDER):
+    os.mkdir(STATIC_FOLDER)
+
+FRAMES_FOLDER = os.path.join(STATIC_FOLDER, 'frames')
+if not os.path.isdir(FRAMES_FOLDER):
+    os.mkdir(FRAMES_FOLDER)
+
+#model = tf.keras.models.model_from_json(
+    #open("/home/rants/PycharmProjects/kbs-project/json_files/object_detection.json", "r").read())
+
+# loading the weights
+#model.load_weights('/home/rants/PycharmProjects/kbs-project/models/object_detection.h5')
+model =VGG16()
+objects = []
+locations = []
+
+# taking in my video input function
+def video_to_images(video):
+    cap = cv2.VideoCapture(video)
+    success, image = cap.read()
+    count = 0
+    while success:
+        cv2.imwrite(os.path.join(STATIC_FOLDER, f'frame{count}.jpg'), image)  # save frame as JPEG file
+        success, image = cap.read()
+        if not success:
+            print('Done')
+        count += 1
+
+
+def search(list, item):
+    for i in range(len(list)):
+        if list[i] == item:
+            return True
+    return False
+
+
+def detect_objects(folder):
+    count = 0
+    for file in os.listdir(folder):
+        if count <= 10:
+            full_path = 'static/frames/' + file
+            image = load_img(full_path, target_size=(224, 224))
+            image = img_to_array(image)
+            image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+            image = preprocess_input(image)
+            y_pred = model.predict(image)
+            labels = decode_predictions(y_pred, top=1)
+            im = cv2.imread(full_path)
+            bbox, label, conf = cv.detect_common_objects(im)
+            output_image = draw_bbox(im, bbox, label, conf)
+            for i in labels[0][0]:
+                if not search(objects, labels[0][0][1]):
+                    objects.append(labels[0][0][1])
+
+            for i in label:
+                if not search(objects, i):
+                    objects.append(i)
+
+            locations.append(full_path)
+            count += 1
+            print('Done')
+        else:
+            break
+
+
+def search_object(item):
+    if search(objects, item):
+        temp = objects.index(item)
+        temp_location = locations[temp]
+        im = cv2.imread(temp_location)
+        bbox, label, conf = cv.detect_common_objects(im)
+        output_image = draw_bbox(im, bbox, label, conf)
+        # output_frame(output_image)
+        print("The object: %s is found in this frame of the video" % item)
+        # print(temp_location)
+        return temp_location
---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+from detection import *
+from tensorflow.keras.models import Sequential, load_model
+
+app = Flask(__name__)
+
+app.secret_key = "secret key"
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+path = os.getcwd()
+
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+STATIC_FOLDER = os.path.join(path, 'static')
+if not os.path.isdir(STATIC_FOLDER):
+    os.mkdir(STATIC_FOLDER)
+
+FRAMES_FOLDER = os.path.join(STATIC_FOLDER, 'frames')
+if not os.path.isdir(FRAMES_FOLDER):
+    os.mkdir(FRAMES_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(['avi', 'mp4', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+video = UPLOAD_FOLDER
+folder = FRAMES_FOLDER
+item = FRAMES_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def upload_form():
+    print("In here")
+    return render_template('upload.html')
+
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+
+    if 'files[]' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    files = request.files.getlist('files[]')
+
+    for file in files:
+        if file and allowed_file(file.filename):
+
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+    flash('File(s) successfully uploaded')
+
+    video_to_images(video)
+    search(list, item)
+    detect_objects(folder)
+
+    return redirect('/')
+
+@app.route('/search_object', methods=["GET"])
+def search_object():
+    print(request.form)
+
+    #Do your magic here ....
+
+    return "Done"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=False, threaded=True)
